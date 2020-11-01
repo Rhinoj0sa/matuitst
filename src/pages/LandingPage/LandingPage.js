c4c08f0c0a797763af56bcafefaef23dd154d2d5
roberto_hinojosa <roberto_hinojosa@hotmail.com>
2020-11-01 02:23:01 -0600

    commit inicial

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Paper from '@material-ui/core/Paper';
+import Image from './THfqmB.jpg'; // Import using relative path
+
+
+const styles = {
+    paperContainer: {
+        backgroundImage: `url(${Image})`
+        
+    }
+};
 
 export default function () {
   return (
-    <div
-      style={{
+      <div style={{
         position: 'absolute',
         left: 0,
         top: 0,
@@ -14,11 +23,13 @@ export default function () {
         justifyContent: 'center',
         alignItems: 'center',
         flexDirection: 'column',
-      }}
-    >
-      <h1>Welcome</h1>
+      }} >
+       <Paper style={styles.paperContainer} xs={12} >
+       <h1>Welcome</h1>
       <h2>CRUD Example Material ui</h2>
 
+        </Paper>
+      
       <div>
         <Link to="/home">Continuar</Link>
       </div>
