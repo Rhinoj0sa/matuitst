fded9ff134814863749895adac8dea753af12200
roberto_hinojosa <roberto_hinojosa@hotmail.com>
2020-11-01 21:26:39 -0600

    landing page

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,38 +1,27 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Paper from '@material-ui/core/Paper';
-import Image from './THfqmB.jpg'; // Import using relative path
-
+import React from "react";
+import { Link } from "react-router-dom";
+import Paper from "@material-ui/core/Paper";
+import Image from "./THfqmB.jpg"; // Import using relative path
+import { makeStyles } from "@material-ui/core/styles";
 
 const styles = {
-    paperContainer: {
-        backgroundImage: `url(${Image})`
-        
-    }
+  root: {
+    height: "100vh",
+    width: "100%",
+    backgroundImage: `url(${Image})`,
+  },
 };
 
-export default function () {
-  return (
-      <div style={{
-        position: 'absolute',
-        left: 0,
-        top: 0,
-        bottom: 0,
-        right: 0,
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'column',
-      }} >
-       <Paper style={styles.paperContainer} xs={12} >
-       <h1>Welcome</h1>
-      <h2>CRUD Example Material ui</h2>
+const useStyles = makeStyles(styles);
 
-        </Paper>
-      
-      <div>
-        <Link to="/home">Continuar</Link>
-      </div>
+export default function () {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Link to="/home">
+        <h1>Welcome</h1>
+        <h2>CRUD Example Material ui</h2>
+      </Link>
     </div>
-  )
+  );
 }
